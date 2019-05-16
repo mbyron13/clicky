d5b0c4819036ea8b76f4d804247cfbb641b16198
Mike B <michael.byron@gmail.com>
2019-05-16 09:35:32 -0400

    Updates

---
+++ precache-manifest.2997ef130cf46cc8dc282dd9aa5ee8c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15677a22a609067eb99070733ca520d9",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "cddf5a93b0cce47276c6",
+    "url": "/clicky/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5eb08805ba029a104c1e",
+    "url": "/clicky/static/css/main.84ec2503.chunk.css"
+  },
+  {
+    "revision": "cddf5a93b0cce47276c6",
+    "url": "/clicky/static/js/2.fcd57da8.chunk.js"
+  },
+  {
+    "revision": "5eb08805ba029a104c1e",
+    "url": "/clicky/static/js/main.da1350c3.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  }
+]);
